311f0d212f84eac3d9b54b4d69c480aca1fbb292
Piyush Bhandari <piyush.bhandari@cubastion.com>
2025-04-23 12:38:04 +0530

    fixed login and cart

--- frontedn-zip/src/customer/Components/Cart/Cart.jsx
+++ frontedn-zip/src/customer/Components/Cart/Cart.jsx
@@ -10,66 +10,71 @@ const Cart = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const jwt = localStorage.getItem("jwt");
-  const {cart}=useSelector(store=>store);
-  console.log("cart ",cart)
+  const { cart } = useSelector((store) => store);
+  // console.log("cart ", cart);
 
   useEffect(() => {
     dispatch(getCart(jwt));
   }, [jwt]);
-  
-    return (
-      <div className="">
-        {cart && cart.cartItems && cart.cartItems.length > 0 ? (
-          <div className="lg:grid grid-cols-3 lg:px-16 relative">
-            <div className="lg:col-span-2 lg:px-5 bg-white">
-              <div className="space-y-3">
-                {cart.cartItems.map((item) => (
-                  <CartItem key={item._id} item={item} showButton={true} />
-                ))}
-              </div>
+
+  return (
+    <div className="">
+      {cart && cart.cartItems && cart.cartItems.length > 0 ? (
+        <div className="relative grid-cols-3 lg:grid lg:px-16">
+          <div className="bg-white lg:col-span-2 lg:px-5">
+            <div className="space-y-3">
+              {cart.cartItems.map((item,index) => {
+                return (
+                  <CartItem key={index} item={item} showButton={true} />
+                );
+              })}
             </div>
-            <div className="px-5 sticky top-0 h-[100vh] mt-5 lg:mt-0 ">
-              <div className="border p-5 bg-white shadow-lg rounded-md">
-                <p className="font-bold opacity-60 pb-4">PRICE DETAILS</p>
-                <hr />
-                <div className="space-y-3 font-semibold">
-                  <div className="flex justify-between pt-3 text-black ">
-                    <span>Price ({cart.cart?.totalItem} item)</span>
-                    <span>₹{cart.cart?.totalPrice}</span>
-                  </div>
-                  <div className="flex justify-between">
-                    <span>Discount</span>
-                    <span className="text-green-700">-₹{cart.cart?.discounte}</span>
-                  </div>
-                  <div className="flex justify-between">
-                    <span>Delivery Charges</span>
-                    <span className="text-green-700">Free</span>
-                  </div>
-                  <hr />
-                  <div className="flex justify-between font-bold text-lg">
-                    <span>Total Amount</span>
-                    <span className="text-green-700">₹{cart.cart?.totalDiscountedPrice}</span>
-                  </div>
+          </div>
+          <div className="px-5 sticky top-0 h-[100vh] mt-5 lg:mt-0 ">
+            <div className="p-5 bg-white border rounded-md shadow-lg">
+              <p className="pb-4 font-bold opacity-60">PRICE DETAILS</p>
+              <hr />
+              <div className="space-y-3 font-semibold">
+                <div className="flex justify-between pt-3 text-black ">
+                  <span>Price ({cart.cart?.totalItem} item)</span>
+                  <span>₹{cart.cart?.totalPrice}</span>
                 </div>
-                <Button
-                  onClick={() => navigate("/checkout?step=2")}
-                  variant="contained"
-                  type="submit"
-                  sx={{ padding: ".8rem 2rem", marginTop: "2rem", width: "100%" }}
-                >
-                  Check Out
-                </Button>
+                <div className="flex justify-between">
+                  <span>Discount</span>
+                  <span className="text-green-700">
+                    -₹{cart.cart?.discounte}
+                  </span>
+                </div>
+                <div className="flex justify-between">
+                  <span>Delivery Charges</span>
+                  <span className="text-green-700">Free</span>
+                </div>
+                <hr />
+                <div className="flex justify-between text-lg font-bold">
+                  <span>Total Amount</span>
+                  <span className="text-green-700">
+                    ₹{cart.cart?.totalDiscountedPrice}
+                  </span>
+                </div>
               </div>
+              <Button
+                onClick={() => navigate("/checkout?step=2")}
+                variant="contained"
+                type="submit"
+                sx={{ padding: ".8rem 2rem", marginTop: "2rem", width: "100%" }}
+              >
+                Check Out
+              </Button>
             </div>
           </div>
-        ) : (
-          <div className="text-center py-20 text-xl font-semibold text-gray-600">
-            No items in your cart 🛒
-          </div>
-        )}
-      </div>
-    );
-    
+        </div>
+      ) : (
+        <div className="py-20 text-xl font-semibold text-center text-gray-600">
+          No items in your cart 🛒
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Cart;
--- frontedn-zip/src/customer/Components/Navbar/Navigation.jsx
+++ frontedn-zip/src/customer/Components/Navbar/Navigation.jsx
@@ -33,7 +33,7 @@ export default function Navigation() {
 
   useEffect(() => {
     if (jwt) {
-      console.log(jwt)
+      // console.log(jwt)
       dispatch(getUser(jwt));
       dispatch(getCart(jwt));
     }
@@ -84,7 +84,7 @@ export default function Navigation() {
   };
 
   return (
-    <div className="bg-white pb-10">
+    <div className="pb-10 bg-white">
       {/* Mobile menu */}
       <Transition.Root show={open} as={Fragment}>
         <Dialog as="div" className="relative z-40 lg:hidden" onClose={setOpen}>
@@ -110,15 +110,15 @@ export default function Navigation() {
               leaveFrom="translate-x-0"
               leaveTo="-translate-x-full"
             >
-              <Dialog.Panel className="relative flex w-full max-w-xs flex-col overflow-y-auto bg-white pb-12 shadow-xl">
-                <div className="flex px-4 pb-2 pt-5">
+              <Dialog.Panel className="relative flex flex-col w-full max-w-xs pb-12 overflow-y-auto bg-white shadow-xl">
+                <div className="flex px-4 pt-5 pb-2">
                   <button
                     type="button"
-                    className="-m-2 inline-flex items-center justify-center rounded-md p-2 text-gray-400"
+                    className="inline-flex items-center justify-center p-2 -m-2 text-gray-400 rounded-md"
                     onClick={() => setOpen(false)}
                   >
                     <span className="sr-only">Close menu</span>
-                    <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                    <XMarkIcon className="w-6 h-6" aria-hidden="true" />
                   </button>
                 </div>
 
@@ -126,7 +126,7 @@ export default function Navigation() {
                 <Tab.Group as="div" className="mt-2">
                   <div className="border-b border-gray-200">
                     {/* console.log(navigation.categories); */}
-                    <Tab.List className="-mb-px flex space-x-8 px-4">
+                    <Tab.List className="flex px-4 -mb-px space-x-8">
                       {navigation?.categories?.map((category) => (
                         <Tab
                           key={category.name}
@@ -148,15 +148,15 @@ export default function Navigation() {
                     {navigation?.categories?.map((category) => (
                       <Tab.Panel
                         key={category.name}
-                        className="space-y-10 px-4 pb-8 pt-10"
+                        className="px-4 pt-10 pb-8 space-y-10"
                       >
                         <div className="grid grid-cols-2 gap-x-4">
                           {category.featured.map((item) => (
                             <div
                               key={item.name}
-                              className="group relative text-sm"
+                              className="relative text-sm group"
                             >
-                              <div className="aspect-h-1 aspect-w-1 overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75">
+                              <div className="overflow-hidden bg-gray-100 rounded-lg aspect-h-1 aspect-w-1 group-hover:opacity-75">
                                 <img
                                   src={item.imageSrc}
                                   alt={item.imageAlt}
@@ -165,7 +165,7 @@ export default function Navigation() {
                               </div>
                               <a
                                 href={item.href}
-                                className="mt-6 block font-medium text-gray-900"
+                                className="block mt-6 font-medium text-gray-900"
                               >
                                 <span
                                   className="absolute inset-0 z-10"
@@ -191,11 +191,11 @@ export default function Navigation() {
                             <ul
                               role="list"
                               aria-labelledby={`${category.id}-${section.id}-heading-mobile`}
-                              className="mt-6 flex flex-col space-y-6"
+                              className="flex flex-col mt-6 space-y-6"
                             >
                               {section?.items?.map((item) => (
                                 <li key={item.name} className="flow-root">
-                                  <p className="-m-2 block p-2 text-gray-500">
+                                  <p className="block p-2 -m-2 text-gray-500">
                                     {item.name}
                                   </p>
                                 </li>
@@ -208,25 +208,25 @@ export default function Navigation() {
                   </Tab.Panels>
                 </Tab.Group>
 
-                <div className="space-y-6 border-t border-gray-200 px-4 py-6">
+                <div className="px-4 py-6 space-y-6 border-t border-gray-200">
                   <div className="flow-root">
                     <a
                       href="/"
-                      className="-m-2 block p-2 font-medium text-gray-900"
+                      className="block p-2 -m-2 font-medium text-gray-900"
                     >
                       Sign in
                     </a>
                   </div>
                 </div>
 
-                <div className="border-t border-gray-200 px-4 py-6">
-                  <a href="/" className="-m-2 flex items-center p-2">
+                <div className="px-4 py-6 border-t border-gray-200">
+                  <a href="/" className="flex items-center p-2 -m-2">
                     <img
                       src="https://tailwindui.com/img/flags/flag-canada.svg"
                       alt=""
-                      className="block h-auto w-5 flex-shrink-0"
+                      className="flex-shrink-0 block w-5 h-auto"
                     />
-                    <span className="ml-3 block text-base font-medium text-gray-900">
+                    <span className="block ml-3 text-base font-medium text-gray-900">
                       CAD
                     </span>
                     <span className="sr-only">, change currency</span>
@@ -239,37 +239,37 @@ export default function Navigation() {
       </Transition.Root>
       {/* change */}
       <header className="relative bg-white">
-        <p className="flex h-10 items-center justify-center bg-indigo-600 px-4 text-sm font-medium text-white sm:px-6 lg:px-8">
+        <p className="flex items-center justify-center h-10 px-4 text-sm font-medium text-white bg-indigo-600 sm:px-6 lg:px-8">
           WelCome To Evkarya
         </p>
 
         <nav aria-label="Top" className="mx-auto">
           <div className="border-b border-gray-200">
-            <div className="flex h-16 items-center px-11">
+            <div className="flex items-center h-16 px-11">
               <button
                 type="button"
-                className="rounded-md bg-white p-2 text-gray-400 lg:hidden"
+                className="p-2 text-gray-400 bg-white rounded-md lg:hidden"
                 onClick={() => setOpen(true)}
               >
                 <span className="sr-only">Open menu</span>
-                <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+                <Bars3Icon className="w-6 h-6" aria-hidden="true" />
               </button>
 
               {/* Logo */}
-              <div className="ml-4 flex lg:ml-0">
+              <div className="flex ml-4 lg:ml-0">
                 <Link to="/">
                   {/* <span className="sr-only">Your Company</span> */}
                   {/* <img
                     src="https://res.cloudinary.com/ddkso1wxi/image/upload/v1675919455/Logo/Copy_of_Zosh_Academy_nblljp.png"
                     alt="Shopwithzosh"
-                    className="h-8 w-8 mr-2"
+                    className="w-8 h-8 mr-2"
                   /> */}
                   <h1>Home</h1>
                 </Link>
               </div>
 
               {/* Flyout menus */}
-              <Popover.Group className="hidden lg:ml-8 lg:block lg:self-stretch z-10">
+              <Popover.Group className="z-10 hidden lg:ml-8 lg:block lg:self-stretch">
                 <div className="flex h-full space-x-8">
                   {navigation.categories.map((category) => (
                     <Popover key={category.name} className="flex">
@@ -297,23 +297,23 @@ export default function Navigation() {
                             leaveFrom="opacity-100"
                             leaveTo="opacity-0"
                           >
-                            <Popover.Panel className="absolute inset-x-0 top-full text-sm text-gray-500">
+                            <Popover.Panel className="absolute inset-x-0 text-sm text-gray-500 top-full">
                               {/* Presentational element used to render the bottom shadow, if we put the shadow on the actual panel it pokes out the top, so we use this shorter element to hide the top of the shadow */}
                               <div
-                                className="absolute inset-0 top-1/2 bg-white shadow"
+                                className="absolute inset-0 bg-white shadow top-1/2"
                                 aria-hidden="true"
                               />
 
                               <div className="relative bg-white">
-                                <div className="mx-auto max-w-7xl px-8">
-                                  <div className="grid grid-cols-2 gap-x-8 gap-y-10 py-16">
-                                    <div className="col-start-2 grid grid-cols-2 gap-x-8">
+                                <div className="px-8 mx-auto max-w-7xl">
+                                  <div className="grid grid-cols-2 py-16 gap-x-8 gap-y-10">
+                                    <div className="grid grid-cols-2 col-start-2 gap-x-8">
                                       {category.featured.map((item) => (
                                         <div
                                           key={item.name}
-                                          className="group relative text-base sm:text-sm"
+                                          className="relative text-base group sm:text-sm"
                                         >
-                                          <div className="aspect-h-1 aspect-w-1 overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75">
+                                          <div className="overflow-hidden bg-gray-100 rounded-lg aspect-h-1 aspect-w-1 group-hover:opacity-75">
                                             <img
                                               src={item.imageSrc}
                                               alt={item.imageAlt}
@@ -322,7 +322,7 @@ export default function Navigation() {
                                           </div>
                                           <a
                                             href={item.href}
-                                            className="mt-6 block font-medium text-gray-900"
+                                            className="block mt-6 font-medium text-gray-900"
                                           >
                                             <span
                                               className="absolute inset-0 z-10"
@@ -339,7 +339,7 @@ export default function Navigation() {
                                         </div>
                                       ))}
                                     </div>
-                                    <div className="row-start-1 grid grid-cols-3 gap-x-8 gap-y-10 text-sm">
+                                    <div className="grid grid-cols-3 row-start-1 text-sm gap-x-8 gap-y-10">
                                       {category.sections.map((section) => (
                                         <div key={section.name}>
                                           <p
@@ -400,17 +400,17 @@ export default function Navigation() {
                 </div>
               </Popover.Group>
 
-              <div className="ml-auto flex items-center">
+              <div className="flex items-center ml-auto">
                 <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
                   {auth.user ? (
                     <div>
                       {/* change */}
                       {/* <button
                         onClick={() => navigate("/cart")}
-                        className="group -m-2 flex items-center p-2"
+                        className="flex items-center p-2 -m-2 group"
                       >
                         <ShoppingBagIcon
-                          className="h-6 w-6 text-gray-400 group-hover:text-gray-500"
+                          className="w-6 h-6 text-gray-400 group-hover:text-gray-500"
                           aria-hidden="true"
                         />
                         {cart.cart?.items?.length > 0 && (
@@ -480,20 +480,20 @@ export default function Navigation() {
                   <p className="p-2 text-gray-400 hover:text-gray-500">
                     <span className="sr-only">Search</span>
                     <MagnifyingGlassIcon
-                      className="h-6 w-6"
+                      className="w-6 h-6"
                       aria-hidden="true"
                     />
                   </p>
                 </div>
 
                 {/* Cart */}
-                <div className="ml-4 flow-root lg:ml-6">
+                <div className="flow-root ml-4 lg:ml-6">
                   <Button
                     onClick={() => navigate("/cart")}
-                    className="group -m-2 flex items-center p-2"
+                    className="flex items-center p-2 -m-2 group"
                   >
                     <ShoppingBagIcon
-                      className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
+                      className="flex-shrink-0 w-6 h-6 text-gray-400 group-hover:text-gray-500"
                       aria-hidden="true"
                     />
                     <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
